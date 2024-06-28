dcda6db493969023d0e57fbe53ed4c36fde9dd70
huynv-3166 <110594805+huynv-3166@users.noreply.github.com>
2024-06-28 15:49:25 +0700

    remove shop

--- Assets/Scripts/ItemShop.cs
+++ Assets/Scripts/ItemShop.cs
@@ -6,7 +6,6 @@ using TMPro;
 
 public class ItemShop : MonoBehaviour
 {
-    [SerializeField] private Button buyButton;
     [SerializeField] private Button useButton;
     [SerializeField] private TextMeshProUGUI useText;
     [SerializeField] private int index;
@@ -17,15 +16,12 @@ public class ItemShop : MonoBehaviour
 
     private void Start()
     {
-        buyButton.onClick.AddListener(Buy);
         useButton.onClick.AddListener(Use);
     }
-    public void Setup(bool isBought, int idx, bool isExplode)
+    public void Setup(int idx, bool isExplode)
     {
         index = idx;
         isExplodeShop = isExplode;
-        buyButton.gameObject.SetActive(!isBought);
-        useButton.gameObject.SetActive(isBought);
         SetIsUsing();
     }
     public void SetIsUsing()
@@ -44,23 +40,10 @@ public class ItemShop : MonoBehaviour
         useText.text = isUsing ? usingString : useString;
         useButton.interactable = isUsing ? false : true;
     }
-    private void Buy()
-    {
-        if (isExplodeShop)
-        {
-            ShopScreen.Instance.BuyExplodeShop(index, 1);
-        }
-        else
-        {
-            ShopScreen.Instance.BuyFloorShop(index, 1);
-        }
-        //Setup(true, index, isExplodeShop);
-    }    
     private void Use()
     {
         string shop = isExplodeShop ? Define.EXPLODE_SHOP : Define.FLOOR_SHOP;
         UserData.Instance.SaveCurrentShop(shop, index);
-        //Setup(true, index, isExplodeShop);
         if (isExplodeShop)
         {
             ShopScreen.Instance.GetExplodeShop();
--- Assets/Scripts/ShopScreen.cs
+++ Assets/Scripts/ShopScreen.cs
@@ -9,8 +9,6 @@ public class ShopScreen : MonoBehaviour
 
     [SerializeField] private List<ItemShop> itemExplodeShops;
     [SerializeField] private List<ItemShop> itemFloorShops;
-    [SerializeField] private List<int> explodeShop;
-    [SerializeField] private List<int> floorShop;
 
     private string explodeString = "exploreShop";
     private string floorString = "floorShop";
@@ -21,75 +19,21 @@ public class ShopScreen : MonoBehaviour
     }
     private void Start()
     {
-        BuyExplodeShop(0, 1);
-        BuyFloorShop(0, 1);
-    }
-    public void BuyExplodeShop(int index, int value)
-    {
-        //SavePrefsInt(explodeString + index, value);
-        PlayerPrefs.SetInt(explodeString + index, value);
-        PlayerPrefs.Save();
-        for (int i = 0; i < explodeShop.Count; i++)
-        {
-            explodeShop[i] = PlayerPrefs.GetInt(explodeString + i);
-            itemExplodeShops[i].Setup(Convert.ToBoolean(explodeShop[i]), i, true);
-        }
-        //LoadPrefs();
-    }
-    public void BuyFloorShop(int index, int value)
-    {
-        //SavePrefsInt(floorString + index, value);
-        PlayerPrefs.SetInt(floorString + index, value);
-        PlayerPrefs.Save();
-        for (int i = 0; i < floorShop.Count; i++)
-        {
-            floorShop[i] = PlayerPrefs.GetInt(floorString + i);
-            itemFloorShops[i].Setup(Convert.ToBoolean(floorShop[i]), i, false);
-        }
+        GetExplodeShop();
+        GetFloorShop();
     }
     public void GetExplodeShop()
     {
-        for (int i = 0; i < explodeShop.Count; i++)
+        for (int i = 0; i < itemExplodeShops.Count; i++)
         {
-            explodeShop[i] = PlayerPrefs.GetInt(explodeString + i);
-            itemExplodeShops[i].Setup(Convert.ToBoolean(explodeShop[i]), i, true);
+            itemExplodeShops[i].Setup(i, true);
         }
     }
     public void GetFloorShop()
     {
-        for (int i = 0; i < floorShop.Count; i++)
+        for (int i = 0; i < itemFloorShops.Count; i++)
         {
-            floorShop[i] = PlayerPrefs.GetInt(floorString + i);
-            itemFloorShops[i].Setup(Convert.ToBoolean(floorShop[i]), i, false);
+            itemFloorShops[i].Setup(i, false);
         }
     }
-    //public void SavePrefsInt(string name, int value)
-    //{
-    //    PlayerPrefs.SetInt(name, value);
-    //    PlayerPrefs.Save();
-    //    LoadPrefs();
-    //}
-    //public void LoadPrefs()
-    //{
-    //    for (int index = 0; index < explodeShop.Count; index++)
-    //    {
-    //        explodeShop[index] = PlayerPrefs.GetInt(explodeString + index);
-    //    }
-    //    for (int index = 0; index < floorShop.Count; index++)
-    //    {
-    //        floorShop[index] = PlayerPrefs.GetInt(floorString + index);
-    //    }
-    //    SetupItemShop();
-    //}
-    //private void SetupItemShop()
-    //{
-    //    for (int index = 0; index < itemExplodeShops.Count; index++)
-    //    {
-    //        itemExplodeShops[index].Setup(Convert.ToBoolean(explodeShop[index]), index, true);
-    //    }
-    //    for (int index = 0; index < itemFloorShops.Count; index++)
-    //    {
-    //        itemFloorShops[index].Setup(Convert.ToBoolean(floorShop[index]), index, false);
-    //    }
-    //}
 }
